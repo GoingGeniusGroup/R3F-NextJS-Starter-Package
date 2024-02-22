2e014f378b919a7d9e3fe280ebf24efd0fec37ee
RiyaShresthaa <riyashrestha279@gmail.com>
2024-02-22 18:18:16 +0545

    added icons in signin page

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { LogosGoogleIcon } from '@/logo/LogosGoogleIcon'
+import { LogosApple } from '@/logo/LogosApple'
+import { LogosFacebook } from '@/logo/LogosFacebook'
 const SignIn = () => {
   return (
     <div className='flex w-full min-h-screen '>
@@ -10,36 +13,55 @@ const SignIn = () => {
             <p className='pt-2 text-white text-sm'>Join for Free.</p>
             <p className='text-yellow-300 text-sm'>One Genius Account for all Genius Platform</p>
           </div>
-          <div className='bg-white rounded-3xl shadow-lg p-5 text-gray-600 md:w-80'>
-            <h1 className='font-bold text-black justify-center text-center text-3xl'> Sign In</h1>
-            <form action='' className='flex flex-col space-y-4 p-3'>
-              <div>
-                <input
-                  type='email'
-                  placeholder='Email'
-                  className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-yellow-300'
-                />
-              </div>
-              <div>
-                <input
-                  type='password'
-                  placeholder='Password'
-                  className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-yellow-300'
-                />
-              </div>
-              <div className='text-align'>
-                <p className='text-blue-500 text-sm'>
-                  <a href='' className='text-start'>
-                    Mobile Sign In
-                  </a>
+          <div className='bg-white rounded-3xl shadow-lg p-5 text-gray-600'>
+            <span style={{ marginRight: '2.75rem' }}>
+              <h1 className='font-bold text-black justify-center text-center text-3xl'> Sign In</h1>
+              <form action='' className='flex flex-col space-y-4 p-3'>
+                <div>
+                  <input
+                    type='email'
+                    placeholder='Email'
+                    className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-yellow-300'
+                  />
+                </div>
+                <div>
+                  <input
+                    type='password'
+                    placeholder='Password'
+                    className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-yellow-300'
+                  />
+                </div>
+                <div className='text-align'>
+                  <p className='text-blue-500 text-sm flex justify-between'>
+                    <a href='' className='text-start' style={{ marginRight: '2.75rem' }}>
+                      Mobile Sign In
+                    </a>
+                    <a href='' className='text-end'>
+                      Forgot Password?
+                    </a>
+                  </p>
+                </div>
 
-                  <a href='' className='text-end'>
-                    Forgot Password?
-                  </a>
-                </p>
+                <button className='inline-block bg-black text-white font-bold rounded-lg px-6 py-2'>Sign In</button>
+              </form>
+              <div className='flex items-center'>
+                <hr className='flex-grow border-t-0 border-b-2 border-black w-5' />
+                <span className='mx-2'>or</span>
+                <hr className='flex-grow border-t-0 border-b-2 border-black w-5' />
               </div>
-              <button className='inline-block bg-black text-white font-bold rounded-lg px-6 py-2'>Sign In</button>
-            </form>
+
+              <div className='justify-center p-5 flex space-x-11'>
+                <a href=''>
+                  <LogosGoogleIcon style={{ fontSize: '24px' }} />
+                </a>
+                <a href=''>
+                  <LogosApple style={{ fontSize: '24px' }} />
+                </a>
+                <a href=''>
+                  <LogosFacebook style={{ fontSize: '24px' }} />
+                </a>
+              </div>
+            </span>
           </div>
         </div>
       </div>
