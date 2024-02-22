cbf6ba51c3257bbb8b6df295f05644dcad471831
gg-ayush <97468901+gg-ayush@users.noreply.github.com>
2024-02-22 19:16:03 +0545

    recharts

--- app/blob/page.jsx
+++ app/blob/page.jsx
@@ -84,6 +84,31 @@ export default function Page() {
         </CardBody>
         </CardContainer>
       </div>
+      {/* <div className='flex items-center justify-center'>
+          <ResponsiveContainer
+           width="50vw" height="50vh">
+        <BarChart
+      
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          
+          <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+          <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+        </BarChart>
+      </ResponsiveContainer>
+        </div> */}
     </>
   )
 }
