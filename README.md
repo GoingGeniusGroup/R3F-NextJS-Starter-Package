9e8afb5a97cab33d0726ef1eb5241baaf71ecf63
notniraj <nirajkarkithapa@gmail.com>
2024-02-23 16:21:02 +0545

    few css changes

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # react3-nextjs
 
 This project was bootstrapped with [`create-r3f-app`](https://github.com/utsuboco/create-r3f-app)
+
+
+
+### Responsive changes need to be made
+### Styling changes need to be made
+### Need color schemes, icons and other resources from org.
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -13,8 +13,8 @@ return (
             <p className='pt-2 text-white text-sm'>Join for Free.</p>
             <p className='text-sm text-white'>One Genius Account for all Genius Platform</p>
         </div>
-        <div className="signup flex flex-col items-center justify-center rounded-t-3xl bg-slate-50 h-4/5 p-10">
-            <div className="card flex flex-col items-center justify-center gap-2 lg:w-1/4 h-auto rounded-2xl">
+        <div className="signup flex flex-col items-center justify-center rounded-t-3xl bg-slate-50 sm:h-1/4 lg:h-4/5 p-10">
+            <div className="card flex flex-col items-center justify-center gap-2 lg:w-2/4 h-auto rounded-2xl">
                 <div className="card-title m-0 rounded-t-2xl p-2 mb-5">
                     <h2 className='pt-2 text-xl text-center'>Signup</h2>
                 </div>
@@ -35,10 +35,10 @@ return (
                     </div>
                 </form>
 
-                <div className='flex items-center'>
-                    <hr className='border-solid'/>
-                    <p className='font-semibold'>or</p>
-                    <hr/>
+                <div className='flex items-end'>
+                    <hr className='border-solid h-1 text-black w-full' />
+                    <p className='font-semibold px-5'>or</p>
+                    <hr className='h-px'/>
                 </div>
                 <div className='flex justify-center gap-16 p-5'>
                     <a href=''>
