d6799924a99545fc94200612ddd05dd260f03b26
Piyush <piyushlekhak@gmail.com>
2024-02-26 14:28:38 +0545

    Updated db and api with gg_id as fk.

--- app/api/skills/[id]/route.js
+++ app/api/skills/[id]/route.js
@@ -3,15 +3,15 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-// Function to update a skill
 export async function PUT(request, { params }) {
   try {
-    const { skill, percentage } = await request.json()
+    const { skill, percentage, gg_id } = await request.json()
     const id = parseInt(params.id)
 
+    // Update the skill
     const updatedSkill = await prisma.skills.update({
       where: { id },
-      data: { skill, percentage },
+      data: { skill, percentage, gg_id },
     })
 
     return NextResponse.json(updatedSkill)
@@ -26,6 +26,7 @@ export async function DELETE(request, { params }) {
   try {
     const id = parseInt(params.id)
 
+    // Delete the skill
     const deletedSkill = await prisma.skills.delete({
       where: { id },
     })
@@ -36,3 +37,38 @@ export async function DELETE(request, { params }) {
     return NextResponse.error('Internal Server Error', 500)
   }
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// // Function to update a skill
+// export async function PUT(request, { params }) {
+//   try {
+//     const { skill, percentage } = await request.json()
+//     const id = parseInt(params.id)
+
+//     const updatedSkill = await prisma.skills.update({
+//       where: { id },
+//       data: { skill, percentage },
+//     })
+
+//     return NextResponse.json(updatedSkill)
+//   } catch (error) {
+//     console.error('Error updating skill', error)
+//     return NextResponse.error('Internal Server Error', 500)
+//   }
+// }
+
+// // Function to delete a skill
+// export async function DELETE(request, { params }) {
+//   try {
+//     const id = parseInt(params.id)
+
+//     const deletedSkill = await prisma.skills.delete({
+//       where: { id },
+//     })
+
+//     return NextResponse.json(deletedSkill)
+//   } catch (error) {
+//     console.error('Error deleting skill', error)
+//     return NextResponse.error('Internal Server Error', 500)
+//   }
+// }
--- app/api/skills/route.js
+++ app/api/skills/route.js
@@ -6,13 +6,23 @@ const prisma = new PrismaClient()
 // Function to create a skill
 export async function POST(request) {
   try {
-    const { skill, percentage } = await request.json()
+    const { skill, percentage, gg_id } = await request.json()
 
-    // Create the skill
+    // Check if the user exists
+    const existingUser = await prisma.users.findUnique({
+      where: { gg_id },
+    })
+
+    if (!existingUser) {
+      return NextResponse.error('User not found', 404)
+    }
+
+    // Create the skill for the user
     const newSkill = await prisma.skills.create({
       data: {
         skill,
         percentage,
+        gg_id,
       },
     })
 
@@ -33,3 +43,35 @@ export async function GET() {
     return NextResponse.error('Internal Server Error', 500)
   }
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// // Function to create a skill
+// export async function POST(request) {
+//   try {
+//     const { skill, percentage } = await request.json()
+
+//     // Create the skill
+//     const newSkill = await prisma.skills.create({
+//       data: {
+//         skill,
+//         percentage,
+//       },
+//     })
+
+//     return NextResponse.json(newSkill)
+//   } catch (error) {
+//     console.error('Error Creating Skill', error)
+//     return NextResponse.error('Internal Server Error', 500)
+//   }
+// }
+
+// // Function to read all skills
+// export async function GET() {
+//   try {
+//     const skills = await prisma.skills.findMany()
+//     return NextResponse.json(skills)
+//   } catch (error) {
+//     console.error('Error fetching skills', error)
+//     return NextResponse.error('Internal Server Error', 500)
+//   }
+// }
