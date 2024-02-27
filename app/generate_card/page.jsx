3a481e761144a7a87849400e571dbcbcda96abdf
RiyaShresthaa <riyashrestha279@gmail.com>
2024-02-27 12:32:29 +0545

    generate card page added

--- app/generate_card/page.jsx
+++ app/generate_card/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { LogosGoogleIcon } from '@/logo/LogosGoogleIcon'
 import { LogosApple } from '@/logo/LogosApple'
 import { LogosFacebook } from '@/logo/LogosFacebook'
-import styles from './signin.module.css'
+import styles from './generate_card.module.css'
 import { CardBody, CardContainer, CardItem } from '@/components/card/card'
 import Image from 'next/image'
 
@@ -12,10 +12,10 @@ const SignIn = () => {
       <div className='flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 bg-black w-full pl-28 pt-5 text-white'>
         <div className='flex flex-col space-y-8 justify-between pt-5'>
           <div>
-            <h1 className='font-bold text-4xl tracking-wide'> GG Users</h1>
+            <h1 className='font-bold text-4xl tracking-wide'> GG Card</h1>
             <h2 className='pt-2 text-white text-xl'>Welcome!</h2>
-            <p className='pt-2 text-white text-sm'>Join for Free.</p>
-            <p className='text-yellow-300 text-sm'>One Genius Account for all Genius Platform</p>
+            <p className='pt-2 text-white text-sm'>Please provide us with your information for your</p>
+            <p className='text-yellow-300 text-sm'>Genius Card</p>
           </div>
           <div>
             <CardContainer className='hover:shadow-3xl dark:border-none dark:hover:border-none dark:hover:shadow-3xl py-0'>
@@ -49,9 +49,23 @@ const SignIn = () => {
           </div>
 
           <div className={styles.container}>
-            <div className='signinform bg-white rounded-3xl shadow-lg p-5 text-gray-600 '>
-              <h1 className='font-bold text-black justify-center text-center text-3xl'> Sign In</h1>
-              <form action='' className='flex flex-col space-y-4 p-3'>
+            <div className='signinform bg-white rounded-3xl shadow-lg p-8 text-gray-600 '>
+              <h1 className='font-bold text-black justify-center text-center text-3xl'> Generate Your Genius Card</h1>
+              <form action='' className='flex flex-col space-y-4 p-5'>
+                <div>
+                  <input
+                    type='Name'
+                    placeholder='Full Name'
+                    className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-yellow-300'
+                  />
+                </div>
+                <div>
+                  <input
+                    type='text'
+                    placeholder='Phone Number'
+                    className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-yellow-300'
+                  />
+                </div>
                 <div>
                   <input
                     type='email'
@@ -61,48 +75,15 @@ const SignIn = () => {
                 </div>
                 <div>
                   <input
-                    type='password'
-                    placeholder='Password'
+                    type='address'
+                    placeholder='Address'
                     className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-yellow-300'
                   />
                 </div>
-                <div className='text-align'>
-                  <p className='text-blue-500 text-sm flex justify-between'>
-                    <a href='' className='text-start' style={{ marginRight: '2.75rem' }}>
-                      Mobile Sign In
-                    </a>
-                    <a href='' className='text-end'>
-                      Forgot Password?
-                    </a>
-                  </p>
-                </div>
 
                 <button className='inline-block bg-black text-white font-bold rounded-lg px-6 py-2 hover:scale-110 transition duration-500 cursor-pointer'>
-                  Sign In
+                  Generate
                 </button>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <hr style={{ flex: '1', border: 'none', borderBottom: '0.5px solid black' }} />
-                  <p style={{ margin: '0 5px' }}>or</p>
-                  <hr style={{ flex: '1', border: 'none', borderBottom: '0.5px solid black' }} />
-                </div>
-
-                <div className='flex space-x-11 justify-center'>
-                  <div className='justify-center p-5 flex hover:scale-125 transition duration-500 cursor-pointer'>
-                    <a href=''>
-                      <LogosGoogleIcon style={{ fontSize: '24px' }} />
-                    </a>
-                  </div>
-                  <div className='justify-center p-5 flex hover:scale-125 transition duration-500 cursor-pointer'>
-                    <a href=''>
-                      <LogosApple style={{ fontSize: '24px' }} />
-                    </a>
-                  </div>
-                  <div className='justify-center p-5 flex hover:scale-125 transition duration-500 cursor-pointer'>
-                    <a href=''>
-                      <LogosFacebook style={{ fontSize: '24px' }} />
-                    </a>
-                  </div>
-                </div>
               </form>
             </div>
           </div>
