cf8b87692ade2b10f6c3f80f591921dad1d3b590
gg-ayush <97468901+gg-ayush@users.noreply.github.com>
2024-02-27 12:51:32 +0545

    skills

--- app/skills/page.jsx
+++ app/skills/page.jsx
@@ -1,14 +1,10 @@
-import Hero from "@/components/Hero/main/Hero";
+
 import Skills from "@/components/Hero/main/Skills";
 
 export default function Home() {
   return (
     <main className="size-full">
       <div className="flex flex-col gap-20">
-        <Hero />
-        {/* <Skills />
-        <Encryption />
-        <Projects /> */}
         <Skills />
       </div>
     </main>
