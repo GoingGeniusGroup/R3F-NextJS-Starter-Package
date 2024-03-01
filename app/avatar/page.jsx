8f79b355fb38de45370d385c71c93a4a381e9ec4
RiyaShresthaa <riyashrestha279@gmail.com>
2024-03-01 12:47:33 +0545

    signin route

--- app/avatar/page.jsx
+++ app/avatar/page.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Avatar_creator from '@/components/avatar-creator/avatar'
 
 export default function Home() {
@@ -10,17 +9,3 @@ export default function Home() {
     </main>
   )
 }
-=======
-import Avatar_creator from "@/components/avatar-creator/avatar";
-
-
-export default function Home() {
-  return (
-    <main className="size-full">
-      <div className="flex flex-col gap-20">
-        <Avatar_creator />
-      </div>
-    </main>
-  );
-}
->>>>>>> 6208f3ee336f874f2d048730ce8c5d4fd6ac143f
