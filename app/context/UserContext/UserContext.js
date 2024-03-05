afa9a04741bfa4af3722c6ef83a8041a5fed6e48
notniraj <nirajkarkithapa@gmail.com>
2024-03-05 10:10:06 +0545

    logout fix

--- app/context/UserContext/UserContext.js
+++ app/context/UserContext/UserContext.js
@@ -6,8 +6,12 @@ const UserContext = createContext();
 export const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
+    const logout = () => {
+        setUser(null)
+    };
+
     return (
-        <UserContext.Provider value={{ user, setUser }}>
+        <UserContext.Provider value={{ user, setUser, logout }}>
         {children}
         </UserContext.Provider>
     );
