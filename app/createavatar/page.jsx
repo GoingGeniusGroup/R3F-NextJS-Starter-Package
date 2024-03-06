eabe4d34904e748db530c83260018ec8e88c2bdd
RiyaShresthaa <riyashrestha279@gmail.com>
2024-03-06 15:14:12 +0545

    avatar redirect changed

--- app/createavatar/page.jsx
+++ app/createavatar/page.jsx
@@ -6,11 +6,11 @@ const CreateAvatar = () => {
       <div className='flex flex-col justify-center items-center h-screen bg-black'>
         <p className='text-white text-5xl mb-8'>Create Your Avatar</p>
         <div className='flex'>
-          <button className='inline-block bg-white text-black font-bold rounded-lg px-6 py-2 mr-4'>
-            <a href='https://gguser.readyplayer.me/avatar?frameApi'>Create Avatar</a>
+          <button className='inline-block bg-white text-black font-bold rounded-lg px-6 py-2 mr-4' route='/avatar'>
+            Create Avatar
           </button>
-          <button className='inline-block bg-[#E5FF25] text-black font-bold rounded-lg px-6 py-2'>
-            <a href='https://gguser.readyplayer.me/avatar?frameApi'>Edit Avatar</a>
+          <button className='inline-block bg-[#E5FF25] text-black font-bold rounded-lg px-6 py-2' route='/avatar'>
+            Edit Avatar
           </button>
         </div>
       </div>
--- src/components/avatar-creator/avatar.tsx
+++ src/components/avatar-creator/avatar.tsx
@@ -1,8 +1,6 @@
 'use client'
 import { AvatarCreator, AvatarCreatorConfig, AvatarExportedEvent } from '@readyplayerme/react-avatar-creator'
 import { Avatar } from '@readyplayerme/visage'
-import axios from 'axios'
-import { useRouter } from 'next/router'
 import { useState } from 'react'
 
 const config: AvatarCreatorConfig = {
@@ -16,41 +14,31 @@ const style = { width: '100%', height: '100vh', border: 'none' }
 
 export default function Avatar_creator() {
   const [avatarUrl, setAvatarUrl] = useState('')
+  const [showNotification, setShowNotification] = useState(false)
+
   const handleOnAvatarExported = (event: AvatarExportedEvent) => {
     setAvatarUrl(event.data.url)
-    const router = useRouter()
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-
-    const handleSubmit = async (event) => {
-      event.preventDefault()
-      const submit = {
-        email,
-        password,
-      }
-
-      console.log('Submit: ', submit)
-
-      try {
-        const { data } = await axios({
-          url: '/api/users',
-          method: 'POST',
-          data: submit,
-        })
-        console.log('Response:', data)
-        if (data != null) {
-          router.push('/signin')
-        }
-      } catch (error) {
-        console.log('Error: ', error)
-      }
-    }
-
-    return (
-      <>
-        <AvatarCreator subdomain='gguser' config={config} style={style} onAvatarExported={handleOnAvatarExported} />
-        {avatarUrl && <Avatar modelSrc={avatarUrl} />}
-      </>
-    )
   }
+
+  const handleNextButtonClick = () => {
+    setShowNotification(true)
+  }
+
+  const handleNotificationClose = () => {
+    setShowNotification(false)
+  }
+
+  return (
+    <>
+      <AvatarCreator subdomain='gguser' config={config} style={style} onAvatarExported={handleOnAvatarExported} />
+      {avatarUrl && <Avatar modelSrc={avatarUrl} />}
+      {showNotification && (
+        <div className='notification'>
+          <p>Avatar URL: {avatarUrl}</p>
+          <button onClick={handleNotificationClose}>Close</button>
+        </div>
+      )}
+      <button onClick={handleNextButtonClick}>Next</button>
+    </>
+  )
 }
