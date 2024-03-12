d69c4e37b761074a952527d2ecd34230f978df72
gg-ayush <97468901+gg-ayush@users.noreply.github.com>
2024-03-12 12:34:06 +0545

    next/router

--- src/components/avatar-creator/avatar.tsx
+++ src/components/avatar-creator/avatar.tsx
@@ -1,9 +1,9 @@
 'use client'
-import { AvatarCreator, AvatarCreatorConfig, AvatarExportedEvent } from '@readyplayerme/react-avatar-creator';
-import { Avatar } from "@readyplayerme/visage";
-import { useState } from "react";
-import axios from 'axios'
 import { useUser } from '@/context/UserContext/UserContext';
+import { AvatarCreator, AvatarCreatorConfig, AvatarExportedEvent } from '@readyplayerme/react-avatar-creator';
+import axios from 'axios';
+import { useRouter } from "next/router"; // Import useRouter from next/router
+import { useState } from "react";
 
 const config: AvatarCreatorConfig = {
   clearCache: false,
@@ -16,46 +16,39 @@ const style = { width: '100%', height: '100vh', border: 'none' };
 
 export default function Avatar_creator() {
   const [avatarUrl, setAvatarUrl] = useState('');
-  const handleOnAvatarExported = (event: AvatarExportedEvent) => {
-    setAvatarUrl(event.data.url);
-  };
-
   const { log } = console
   const { user } = useUser();
-  // const userId = user.gg_id;
-  console.log(avatarUrl)
-  console.log(user)
+  const userId = user.gg_id;
+  const router = useRouter(); // Initialize useRouter
 
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault()
-  //   const submit = {
-  //     avatarUrl
-  //   }
+  const handleOnAvatarExported = async (event: AvatarExportedEvent) => {
+    const exportedAvatarUrl = event.data.url;
+    setAvatarUrl(exportedAvatarUrl);
 
-  //   log('Submit: ', submit)
+    const submit = {
+      avatarUrl: exportedAvatarUrl
+    };
 
-  //   try {
-  //     const { data } = await axios({
-  //       url: `/api/avatar/${userId}`,
-  //       method: 'PUT',
-  //       data: submit,
-  //     })
-  //     log('Response:', data)
-  //   } catch (error) {
-  //     log('Error: ', error)
-  //   }
-  // }
+    log('Submit: ', submit);
+
+    try {
+      const { data } = await axios({
+        url: `/api/avatar/${userId}`,
+        method: 'PUT',
+        data: submit,
+      })
+      log('Response:', data);
+
+      // Redirect to another page after successful avatar export
+      router.push("/another-page"); // Change "/another-page" to the desired URL
+    } catch (error) {
+      log('Error: ', error)
+    }
+  };
 
   return (
       <>
         <AvatarCreator subdomain="gguser" config={config} style={style} onAvatarExported={handleOnAvatarExported} />
-        {avatarUrl && <Avatar modelSrc={avatarUrl} />}
-        <button 
-          className='text-white p-10'
-          // onClick={handleSubmit}
-        >
-          Next
-        </button>
       </>
   );
 }
