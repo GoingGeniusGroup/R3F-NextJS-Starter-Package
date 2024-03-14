d1808c08fd7e2fbb5bb6a025a7612b96839cf4b7
gg-ayush <97468901+gg-ayush@users.noreply.github.com>
2024-03-14 15:37:41 +0545

    cesium-1

--- next.config.js
+++ next.config.js
@@ -1,79 +1,78 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
 /**
  * A fork of 'next-pwa' that has app directory support
  * @see https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1332258575
  */
 const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'src',
+  dest: 'public',
   disable: process.env.NODE_ENV === 'development',
-})
+});
 
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
-const nextConfig = {
-  // uncomment the following snippet if using styled components
-  // compiler: {
-  //   styledComponents: true,
-  // },
-  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  images: {
-    domains: ['models.readyplayer.me'] 
-  },
-  webpack(config, { webpack, isServer }) {
-    if (!isServer) {
+module.exports = (phase, { defaultConfig }) => {
+  const nextConfig = {
+    // uncomment the following snippet if using styled components
+    // compiler: {
+    //   styledComponents: true,
+    // },
+    reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+    images: {
+      domains: ['models.readyplayer.me'] 
+    },
+    webpack(config, { webpack, isServer }) {
+      if (!isServer) {
+        config.plugins.push(
+          new CopyWebpackPlugin({
+            patterns: [
+              {
+                from: path.join(
+                  __dirname,
+                  'node_modules/cesium/Build/Cesium/Workers'
+                ),
+                to: '../public/Cesium/Workers',
+              },
+              {
+                from: path.join(
+                  __dirname,
+                  'node_modules/cesium/Build/Cesium/ThirdParty'
+                ),
+                to: '../public/Cesium/ThirdParty',
+              },
+              {
+                from: path.join(
+                  __dirname,
+                  'node_modules/cesium/Build/Cesium/Assets'
+                ),
+                to: '../public/Cesium/Assets',
+              },
+              {
+                from: path.join(
+                  __dirname,
+                  'node_modules/cesium/Build/Cesium/Widgets'
+                ),
+                to: '../public/Cesium/Widgets',
+              },
+            ],
+          })
+        )
+      }
       config.plugins.push(
-        new CopyWebpackPlugin({
-          patterns: [
-            {
-              from: path.join(
-                __dirname,
-                'node_modules/cesium/Build/Cesium/Workers'
-              ),
-              to: '../src/Cesium/Workers',
-            },
-            {
-              from: path.join(
-                __dirname,
-                'node_modules/cesium/Build/Cesium/ThirdParty'
-              ),
-              to: '../src/Cesium/ThirdParty',
-            },
-            {
-              from: path.join(
-                __dirname,
-                'node_modules/cesium/Build/Cesium/Assets'
-              ),
-              to: '../src/Cesium/Assets',
-            },
-            {
-              from: path.join(
-                __dirname,
-                'node_modules/cesium/Build/Cesium/Widgets'
-              ),
-              to: '../src/Cesium/Widgets',
-            },
-          ],
+        new webpack.DefinePlugin({
+          CESIUM_BASE_URL: JSON.stringify('/Cesium'),
         })
       )
-    }
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        CESIUM_BASE_URL: JSON.stringify('/Cesium'),
-      })
-    )
-    config.resolve.exportsFields = []
-    // return {...config, resolve: {...config.resolve, exportsFields:[]}}
-    return config
-  },
-}
+      config.resolve.exportsFields = []
+      return config
+    },
+  };
 
-const KEYS_TO_OMIT = ['webpackDevMiddleware', 'configOrigin', 'target', 'analyticsId', 'webpack5', 'amp', 'assetPrefix']
+  const KEYS_TO_OMIT = ['webpackDevMiddleware', 'configOrigin', 'target', 'analyticsId', 'webpack5', 'amp', 'assetPrefix'];
 
-module.exports = (_phase, { defaultConfig }) => {
   const plugins = [[withPWA], [withBundleAnalyzer, {}]];
 
   const wConfig = plugins.reduce((acc, [plugin, config]) => plugin({ ...acc, ...config }), {
@@ -96,6 +95,6 @@ module.exports = (_phase, { defaultConfig }) => {
       finalConfig[key] = wConfig[key];
     }
   });
-  
+
   return finalConfig;
 };
