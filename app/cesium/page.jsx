212cf6f645228e2fadb3249c433ee8587f7c6b25
gg-ayush <97468901+gg-ayush@users.noreply.github.com>
2024-03-14 14:50:19 +0545

    cesium

---
+++ app/cesium/page.jsx
@@ -0,0 +1,50 @@
+'use client'
+import { Cartesian3, Ion, Math, Terrain, Viewer, createOsmBuildingsAsync } from "cesium";
+import "cesium/Build/Cesium/Widgets/widgets.css";
+import { useEffect } from "react";
+import "./css/main.css";
+
+export default function Cesium() {
+  useEffect(() => {
+    const initializeCesiumViewer = async () => {
+      // CesiumJS has a default access token built in but it's not meant for active use.
+      // Please set your own access token can be found at: https://cesium.com/ion/tokens.
+      // Ion.defaultAccessToken = "YOUR TOKEN HERE";
+      
+      Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkZGUzY2FhOC00M2ViLTQ2ZmQtYWQ1Yy1kYzNhYzFhZmVkZjIiLCJpZCI6MjAwOTU1LCJpYXQiOjE3MTAyMjkxNDF9.09cBca1kjkwB2lSOjuJMFMjOUV1DWT75cHXqT3zGxIU';
+      // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+      const viewer = new Viewer("cesiumContainer", {
+        terrain: Terrain.fromWorldTerrain(),
+      });
+
+      // Add Cesium OSM Buildings, a global 3D buildings layer.
+      const osmBuildingsTileset = await createOsmBuildingsAsync();
+      viewer.scene.primitives.add(osmBuildingsTileset);
+
+      // Fly the camera to San Francisco at the given longitude, latitude, and height.
+      viewer.camera.flyTo({
+        destination: Cartesian3.fromDegrees(85.32, 27.70, 2400),
+        orientation: {
+          heading: Math.toRadians(0.0),
+          pitch: Math.toRadians(-15.0),
+        },
+      });
+    };
+
+    initializeCesiumViewer();
+
+    // Cleanup function (optional)
+    return () => {
+      // Any cleanup code if needed
+    };
+  }, []); // Empty dependency array to run the effect only once
+  return (
+    <>
+      <div className="absolute size-96 bg-black"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sapiente laboriosam quod veniam praesentium, natus voluptas eos, quis explicabo culpa iure dolor earum asperiores nostrum unde suscipit autem excepturi inventore?</p></div>
+      <div id="cesiumContainer" className="h-screen w-full" >
+        {/* Cesium Viewer container */}
+      </div>
+    </>
+  )
+}
+
--- next.config.js
+++ next.config.js
@@ -11,6 +11,9 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   disable: process.env.NODE_ENV === 'development',
 })
 
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
 const nextConfig = {
   // uncomment the following snippet if using styled components
   // compiler: {
@@ -20,45 +23,57 @@ const nextConfig = {
   images: {
     domains: ['models.readyplayer.me'] 
   },
-  webpack(config, { isServer }) {
+  webpack(config, { webpack, isServer }) {
     if (!isServer) {
-      // We're in the browser build, so we can safely exclude the sharp module
-      config.externals.push('sharp')
+      config.plugins.push(
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: path.join(
+                __dirname,
+                'node_modules/cesium/Build/Cesium/Workers'
+              ),
+              to: '../public/Cesium/Workers',
+            },
+            {
+              from: path.join(
+                __dirname,
+                'node_modules/cesium/Build/Cesium/ThirdParty'
+              ),
+              to: '../public/Cesium/ThirdParty',
+            },
+            {
+              from: path.join(
+                __dirname,
+                'node_modules/cesium/Build/Cesium/Assets'
+              ),
+              to: '../public/Cesium/Assets',
+            },
+            {
+              from: path.join(
+                __dirname,
+                'node_modules/cesium/Build/Cesium/Widgets'
+              ),
+              to: '../public/Cesium/Widgets',
+            },
+          ],
+        })
+      )
     }
-    // audio support
-    config.module.rules.push({
-      test: /\.(ogg|mp3|wav|mpe?g)$/i,
-      exclude: config.exclude,
-      use: [
-        {
-          loader: require.resolve('url-loader'),
-          options: {
-            limit: config.inlineImageLimit,
-            fallback: require.resolve('file-loader'),
-            publicPath: `${config.assetPrefix}/_next/static/images/`,
-            outputPath: `${isServer ? '../' : ''}static/images/`,
-            name: '[name]-[hash].[ext]',
-            esModule: config.esModule || false,
-          },
-        },
-      ],
-    })
-
-    // shader support
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      exclude: /node_modules/,
-      use: ['raw-loader', 'glslify-loader'],
-    })
-
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        CESIUM_BASE_URL: JSON.stringify('/Cesium'),
+      })
+    )
+    config.resolve.exportsFields = []
+    // return {...config, resolve: {...config.resolve, exportsFields:[]}}
     return config
   },
 }
 
 const KEYS_TO_OMIT = ['webpackDevMiddleware', 'configOrigin', 'target', 'analyticsId', 'webpack5', 'amp', 'assetPrefix']
-const path = require('path');
+
 module.exports = (_phase, { defaultConfig }) => {
-  
   const plugins = [[withPWA], [withBundleAnalyzer, {}]];
 
   const wConfig = plugins.reduce((acc, [plugin, config]) => plugin({ ...acc, ...config }), {
@@ -76,7 +91,6 @@ module.exports = (_phase, { defaultConfig }) => {
   const finalConfig = { ...wConfig, ...additionalConfig };
 
   // Optionally, if you want to omit certain keys
-  const KEYS_TO_OMIT = []; // Add keys you want to omit here if needed
   Object.keys(finalConfig).forEach((key) => {
     if (!KEYS_TO_OMIT.includes(key)) {
       finalConfig[key] = wConfig[key];
