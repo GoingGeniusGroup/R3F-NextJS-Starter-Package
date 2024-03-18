60c1187ef1c240713e706eeb4939d354ff53b860
gg-ayush <97468901+gg-ayush@users.noreply.github.com>
2024-03-18 12:22:34 +0545

    avatar-creator-navigation

--- src/components/avatar-creator/avatar.tsx
+++ src/components/avatar-creator/avatar.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { AvatarCreator, AvatarCreatorConfig, AvatarExportedEvent } from '@readyplayerme/react-avatar-creator';
-import { Avatar } from "@readyplayerme/visage";
+import { useRouter } from 'next/navigation';
+
 import { useState } from "react";
 
 const config: AvatarCreatorConfig = {
@@ -12,16 +13,17 @@ const config: AvatarCreatorConfig = {
 
 const style = { width: '100%', height: '100vh', border: 'none' };
 
-export default function Avatar_creator() {
+export default function App() {
   const [avatarUrl, setAvatarUrl] = useState('');
+  const router = useRouter()
   const handleOnAvatarExported = (event: AvatarExportedEvent) => {
+    router.push('/hero')
     setAvatarUrl(event.data.url);
   };
 
   return (
       <>
         <AvatarCreator subdomain="gguser" config={config} style={style} onAvatarExported={handleOnAvatarExported} />
-        {avatarUrl && <Avatar modelSrc={avatarUrl} />}
       </>
   );
 }
